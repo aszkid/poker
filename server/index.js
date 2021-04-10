fd82f12025883822b38f2fb549f1b9e92221000d
Pol Gómez Riquelme <pol@duolingo.com>
2021-04-10 18:36:30 +0200

    Implement host-matching with socketio server

--- server/index.js
+++ server/index.js
@@ -3,20 +3,42 @@ const app = require("express")();
 const http = require("http");
 const server = http.createServer(app);
 const io = require("socket.io")(server);
+const randomWords = require("random-words");
+
+// Mapping of server name to server info object
+const servers = {};
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname , "index.html"));
 });
 
 io.on("connection", socket => {
-    socket.on("sdp-advertise", msg => {
-        console.log(`advertising SDP:`, msg);
+    socket.on("server-create", msg => {
+        const name = randomWords(3).join("-");
+        servers[name] = { owner: socket, name };
+        socket.emit("on-server-create", name);
+    });
+    socket.on("sdp-offer", ({ id, offer }) => {
+        const server = servers[id];
+        if (socket.id === server.owner.id) {
+            server.offer = offer;
+        }
+    });
+    socket.on("sdp-answer", ({ id, answer }) => {
+        const server = servers[id];
+        if (server) {
+            server.owner.emit("on-client-connect", answer);
+        }
+    });
+    socket.on("server-connect", id => {
+        const server = servers[id];
+        if (server) {
+            socket.emit("on-server-connect", { offer: server.offer });
+        }
     });
 });
-
 
 const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Listening on ${PORT}...`);
 });
-
